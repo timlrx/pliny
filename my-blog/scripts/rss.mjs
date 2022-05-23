236c81c82ecedac89ac20ffa34450a1593499282
Timothy Lin <timothy0336@hotmail.com>
2022-05-23 23:21:54 +0800

    remove local test blog

--- my-blog/scripts/rss.mjs
+++
@@ -1,5 +0,0 @@
-import generateRss from 'pliny/utils/generate-rss.mjs'
-import siteMetadata from '../data/siteMetadata.js'
-import { allBlogs } from '../.contentlayer/generated/index.json'
-
-generateRss(siteMetadata, allBlogs)
--- my-blog/scripts/sitemap.mjs
+++
@@ -1,5 +0,0 @@
-import generateSitemap from 'pliny/utils/generate-sitemap.mjs'
-import siteMetadata from '../data/siteMetadata.js'
-import { allBlogs } from '../.contentlayer/generated/index.json'
-
-generateSitemap(siteMetadata.siteUrl, allBlogs)
