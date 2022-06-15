7aad25c201e5546a1ead3d92fbe8eb53863864bd
Timothy Lin <timothy0336@hotmail.com>
2022-06-15 23:47:11 +0800

    sync with v1.5.6

--- recipes/blog-classic/templates/layouts/ListLayout.tsx
+++ recipes/blog-classic/templates/layouts/ListLayout.tsx
@@ -75,7 +75,7 @@ export default function ListLayout({
 
   return (
     <>
-      <div className="divide-y">
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="md:leading-14 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl">
             {title}
