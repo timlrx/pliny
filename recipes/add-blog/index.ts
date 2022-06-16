e99552eb24a21ed73d8681f73220c378673c2265
Timothy Lin <timothy0336@hotmail.com>
2022-06-16 23:42:11 +0800

    add contentlayer transform codemod to recipe

--- recipes/add-blog/index.ts
+++ recipes/add-blog/index.ts
@@ -1,5 +1,27 @@
-import { RecipeBuilder } from '@pliny/installer'
+import { RecipeBuilder, paths, addContentlayerDocumentType } from '@pliny/installer'
 import { join } from 'path'
+import * as fs from 'fs'
+
+const BlogDocumentType = {
+  name: 'Blog',
+  filePathPattern: 'blog/**/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    tags: { type: 'list', of: { type: 'string' } },
+    lastmod: { type: 'date' },
+    draft: { type: 'boolean' },
+    summary: { type: 'string' },
+    images: { type: 'list', of: { type: 'string' } },
+    authors: { type: 'list', of: { type: 'string' } },
+    layout: { type: 'string' },
+    bibliography: { type: 'string' },
+    canonicalUrl: { type: 'string' },
+  },
+  // Kindly use your computedFields object or copy it from one of the starter templates
+  computedFields: {},
+}
 
 export default RecipeBuilder()
   .setName('Add blog')
@@ -26,8 +48,45 @@ export default RecipeBuilder()
     explanation: `Adding pages...`,
     targetDirectory: '.',
     templatePath: join(__dirname, 'templates'),
-    templateValues: (args) => {
-      return args
+    templateValues: ({ ContentName, ...rest }) => {
+      // @ts-ignore
+      return { ContentName: ContentName.toLowerCase(), ...rest }
     },
+  })
+  .addTransformFilesStep({
+    stepId: 'updateContentlayer',
+    stepName: 'Add Blog document type to contentlayer',
+    explanation: 'Adds the blog document type to contentlayer.config',
+    singleFileSearch: paths.contentlayerConfig(),
+    transform: (program, args) => {
+      const name = args.ContentName as string
+      return addContentlayerDocumentType(program, name, {
+        name: name,
+        // @ts-ignore
+        filePathPattern: `${name.toLowerCase()}/**/*.mdx`,
+        contentType: 'mdx',
+        fields: {
+          title: { type: 'string', required: true },
+          date: { type: 'date', required: true },
+          tags: { type: 'list', of: { type: 'string' } },
+          lastmod: { type: 'date' },
+          draft: { type: 'boolean' },
+          summary: { type: 'string' },
+          images: { type: 'list', of: { type: 'string' } },
+          authors: { type: 'list', of: { type: 'string' } },
+          layout: { type: 'string' },
+          bibliography: { type: 'string' },
+          canonicalUrl: { type: 'string' },
+        },
+        // Kindly use your computedFields object or copy it from one of the starter templates
+        computedFields: {},
+      })
+    },
+  })
+  .printMessage({
+    stepId: 'manualStep',
+    stepName: 'Please substitute computedFields or copy it from one of the starter templates',
+    message:
+      'Kindly substitute the computedFields object with an existing one or copy it from one of the starter templates',
   })
   .build()
