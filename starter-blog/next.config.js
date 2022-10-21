63e0e1aaade15168f47fb4e988d19a670c1b9cb6
Timothy Lin <timothy0336@hotmail.com>
2022-10-21 16:42:45 +0800

    remove preact

--- starter-blog/next.config.js
+++ starter-blog/next.config.js
@@ -79,13 +79,6 @@ module.exports = () => {
         use: ['@svgr/webpack'],
       })
 
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-
       return config
     },
   })
