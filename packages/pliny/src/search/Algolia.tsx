459c5d85cbb5ea93ced553fe195080d86551558c
Timothy Lin <timothy0336@hotmail.com>
2023-07-16 14:57:30 +0800

    replace next/router with next/navigation

--- packages/pliny/src/search/Algolia.tsx
+++ packages/pliny/src/search/Algolia.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useRef, useCallback } from 'react'
-import { AnchorHTMLAttributes, DetailedHTMLProps } from 'react'
+import { AnchorHTMLAttributes } from 'react'
 import { createPortal } from 'react-dom'
-import { useRouter } from 'next/router.js'
+import { useRouter } from 'next/navigation.js'
 import Link from 'next/link.js'
 import Head from 'next/head.js'
 import { useDocSearchKeyboardEvents } from '@docsearch/react'
+import type { LinkProps } from 'next/link'
 import type {
   DocSearchModal as DocSearchModalType,
   DocSearchProps,
@@ -32,19 +33,12 @@ export interface AlgoliaSearchContext {
 
 let DocSearchModal: typeof DocSearchModalType | null = null
 
-const CustomLink = ({
-  href,
-  ...rest
-}: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => {
+const CustomLink = ({ href, ...rest }: LinkProps & AnchorHTMLAttributes<HTMLAnchorElement>) => {
   const isInternalLink = href && href.startsWith('/')
   const isAnchorLink = href && href.startsWith('#')
 
   if (isInternalLink) {
-    return (
-      <Link href={href}>
-        <a {...rest} />
-      </Link>
-    )
+    return <Link href={href} {...rest} />
   }
 
   if (isAnchorLink) {
@@ -63,137 +57,6 @@ function Hit({
 }) {
   return <CustomLink href={hit.url}>{children}</CustomLink>
 }
-
-// export function AlgoliaSearch({
-//   ButtonChildren,
-//   algoliaConfig,
-//   className: buttonClassName,
-// }: AlgoliaSearchProps) {
-//   const router = useRouter()
-//   const searchButtonRef = useRef<HTMLButtonElement>(null)
-//   const [isOpen, setIsOpen] = useState(false)
-//   const [initialQuery, setInitialQuery] = useState<string | undefined>(undefined)
-
-//   const importDocSearchModalIfNeeded = useCallback(() => {
-//     if (DocSearchModal) {
-//       return Promise.resolve()
-//     }
-
-//     return Promise.all([
-//       import('./AlgoliaModal'),
-//       //   import('@docsearch/react/style'),
-//     ]).then(([{ default: Modal }]) => {
-//       DocSearchModal = Modal
-//     })
-//   }, [])
-
-//   const onOpen = useCallback(() => {
-//     importDocSearchModalIfNeeded().then(() => {
-//       setIsOpen(true)
-//     })
-//   }, [importDocSearchModalIfNeeded, setIsOpen])
-
-//   const onClose = useCallback(() => {
-//     setIsOpen(false)
-//   }, [setIsOpen])
-
-//   const onInput = useCallback(
-//     (event: KeyboardEvent) => {
-//       importDocSearchModalIfNeeded().then(() => {
-//         setIsOpen(true)
-//         setInitialQuery(event.key)
-//       })
-//     },
-//     [importDocSearchModalIfNeeded, setIsOpen, setInitialQuery]
-//   )
-
-//   const navigator = useRef({
-//     navigate({ itemUrl }: { itemUrl?: string }) {
-//       // Algolia results could contain URL's from other domains which cannot
-//       // be served through history and should navigate with window.location
-//       const isInternalLink = itemUrl.startsWith('/')
-//       const isAnchorLink = itemUrl.startsWith('#')
-//       if (!isInternalLink && !isAnchorLink) {
-//         window.location.href = itemUrl
-//       } else {
-//         router.push(itemUrl)
-//       }
-//     },
-//   }).current
-
-//   const transformItems = useRef<DocSearchModalProps['transformItems']>((items) =>
-//     items.map((item) => {
-//       // If Algolia contains a external domain, we should navigate without
-//       // relative URL
-//       const isInternalLink = item.url.startsWith('/')
-//       const isAnchorLink = item.url.startsWith('#')
-//       if (!isInternalLink && !isAnchorLink) {
-//         return item
-//       }
-
-//       // We transform the absolute URL into a relative URL.
-//       const url = new URL(item.url)
-//       return {
-//         ...item,
-//         // url: withBaseUrl(`${url.pathname}${url.hash}`),
-//         url: `${url.pathname}${url.hash}`,
-//       }
-//     })
-//   ).current
-
-//   const SearchButton = useCallback(() => {
-//     return (
-//       <button
-//         aria-label="Search"
-//         onTouchStart={importDocSearchModalIfNeeded}
-//         onFocus={importDocSearchModalIfNeeded}
-//         onMouseOver={importDocSearchModalIfNeeded}
-//         onClick={onOpen}
-//         className={buttonClassName}
-//       >
-//         <ButtonChildren />
-//       </button>
-//     )
-//   }, [ButtonChildren, importDocSearchModalIfNeeded, onOpen])
-
-//   useDocSearchKeyboardEvents({
-//     isOpen,
-//     onOpen,
-//     onClose,
-//     onInput,
-//     searchButtonRef,
-//   })
-
-//   return (
-//     <>
-//       <Head>
-//         {/* This hints the browser that the website will load data from Algolia,
-//         and allows it to preconnect to the DocSearch cluster. It makes the first
-//         query faster, especially on mobile. */}
-//         <link
-//           rel="preconnect"
-//           href={`https://${algoliaConfig.appId}-dsn.algolia.net`}
-//           crossOrigin="anonymous"
-//         />
-//       </Head>
-//       {ButtonChildren && <SearchButton />}
-//       {isOpen &&
-//         DocSearchModal &&
-//         createPortal(
-//           <DocSearchModal
-//             onClose={onClose}
-//             initialScrollY={window.scrollY}
-//             initialQuery={initialQuery}
-//             navigator={navigator}
-//             transformItems={transformItems}
-//             hitComponent={Hit}
-//             {...algoliaConfig}
-//           />,
-//           document.body
-//         )}
-//     </>
-//   )
-// }
 
 export const AlgoliaSearchContext = React.createContext<AlgoliaSearchContext>(
   {} as AlgoliaSearchContext
