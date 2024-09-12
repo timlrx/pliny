0fe6eb3b5f6fb398168e5d784126e6fd9f82b16f
Sahil <sahil@Sahils-MacBook-Air.local>
2024-09-12 14:21:13 -0400

    update URL in actual fetch

--- packages/pliny/src/newsletter/beehiiv.ts
+++ packages/pliny/src/newsletter/beehiiv.ts
@@ -3,14 +3,13 @@ export const beehiivSubscribe = async (email: string) => {
   const PUBLICATION_ID = process.env.BEEHIIV_PUBLICATION_ID
   const API_URL = `https://api.beehiiv.com/v2/publications/${PUBLICATION_ID}/subscriptions`
 
-
   const data = {
     email,
     reactivate_existing: false,
     send_welcome_email: true,
   }
 
-  const response = await fetch(`${API_URL}/subscribers`, {
+  const response = await fetch(API_URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
