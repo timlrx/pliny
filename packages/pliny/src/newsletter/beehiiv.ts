2e35329642817927af61489e93ca71dddbd99595
Sahil <sahil@Sahils-MacBook-Air.local>
2024-09-12 14:14:01 -0400

    updated url of beehiiv API endpoin

--- packages/pliny/src/newsletter/beehiiv.ts
+++ packages/pliny/src/newsletter/beehiiv.ts
@@ -1,11 +1,11 @@
 export const beehiivSubscribe = async (email: string) => {
   const API_KEY = process.env.BEEHIIV_API_KEY
   const PUBLICATION_ID = process.env.BEEHIIV_PUBLICATION_ID
-  const API_URL = 'https://api.beehiiv.com/v2'
+  const API_URL = `https://api.beehiiv.com/v2/publications/${PUBLICATION_ID}/subscriptions`
+
 
   const data = {
     email,
-    publication_id: PUBLICATION_ID,
     reactivate_existing: false,
     send_welcome_email: true,
   }
