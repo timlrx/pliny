e7163786422fb28f974dc39a1fa8b42270d6a6c2
Qian <lsq1076603184@163.com>
2024-11-08 09:53:56 +0800

    fix: change buttondown api params email to email_address

--- packages/pliny/src/newsletter/buttondown.ts
+++ packages/pliny/src/newsletter/buttondown.ts
@@ -1,10 +1,10 @@
-export const buttondownSubscribe = async (email: string) => {
+export const buttondownSubscribe = async (email_address: string) => {
   const API_KEY = process.env.BUTTONDOWN_API_KEY
   const API_URL = 'https://api.buttondown.email/v1/'
   const buttondownRoute = `${API_URL}subscribers`
   const response = await fetch(buttondownRoute, {
     body: JSON.stringify({
-      email,
+      email_address,
     }),
     headers: {
       Authorization: `Token ${API_KEY}`,
