e29e7761a717c80de204db3caaee749385972065
Qian <lsq1076603184@163.com>
2024-11-14 10:10:26 +0800

    keep it to be consistent with the other newsletter APIs

--- packages/pliny/src/newsletter/buttondown.ts
+++ packages/pliny/src/newsletter/buttondown.ts
@@ -1,11 +1,12 @@
-export const buttondownSubscribe = async (email_address: string) => {
+export const buttondownSubscribe = async (email: string) => {
   const API_KEY = process.env.BUTTONDOWN_API_KEY
   const API_URL = 'https://api.buttondown.email/v1/'
   const buttondownRoute = `${API_URL}subscribers`
+
+  const data = { email_address: email }
+
   const response = await fetch(buttondownRoute, {
-    body: JSON.stringify({
-      email_address,
-    }),
+    body: JSON.stringify(data),
     headers: {
       Authorization: `Token ${API_KEY}`,
       'Content-Type': 'application/json',
